b0dab2d00de9882e044ce51a45906de04c52681b
GavFior <gavin@woodsofplenty.com>
2020-11-12 13:56:51 -0800

    footer php

---
+++ scripts/footer.php
@@ -0,0 +1,90 @@
+
+    <!-- footer start -->
+    <footer class="footer">
+        <div class="footer_top">
+            <div class="container">
+                <div class="row">
+                    <div class="col-xl-4 col-md-6 col-lg-4">
+                        <div class="footer_widget">
+                            <div class="footer_logo">
+                                <a href="#">
+                                    <img style="width: 70%" src="img/logo.png" alt="">
+                                </a>
+                            </div>
+                            <p>
+                                Experiece the speed. Get desired results. 
+                            </p>
+                            <div class="socail_links">
+                                <ul>
+                                    <li>
+                                        <a href="#">
+                                            <i class="ti-facebook"></i>
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="#">
+                                            <i class="ti-twitter-alt"></i>
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="#">
+                                            <i class="fa fa-instagram"></i>
+                                        </a>
+                                    </li>
+                                </ul>
+                            </div>
+
+                        </div>
+                    </div>
+                    <div class="col-xl-2 offset-xl-1 col-md-6 col-lg-3">
+                        <div class="footer_widget">
+                            <h3 class="footer_title">
+                                    Services
+                            </h3>
+                            <ul>
+                                <li><a href="#">Team management</a></li>
+                                <li><a href="#">Collaboration</a></li>
+                                <li><a href="#">Todo</a></li>
+                                <li><a href="#">Events</a></li>
+                            </ul>
+
+                        </div>
+                    </div>
+                    <div class="col-xl-2 col-md-6 col-lg-2">
+                        <div class="footer_widget">
+                            <h3 class="footer_title">
+                                    Useful Links
+                            </h3>
+                            <ul>
+                                <li><a href="#">Pricing</a></li>
+                                <li><a href="Process.html">Process</a></li>
+                                <li><a href="#">Blog</a></li>
+                                <li><a href="#">Contact</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-xl-3 col-md-6 col-lg-3">
+                        <div class="footer_widget">
+                            <h3 class="footer_title">
+                                    Downloads
+                            </h3>
+                            <ul>
+                                <li>
+                                    <a href="#">
+                                        Download from Apple Store
+
+                                    </a>
+                                </li>
+                                <li><a href="#">
+                                        Download from Play Store
+                                    </a>
+                                </li>
+                                </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+       
+    </footer>
+    <!--/ footer end  -->
