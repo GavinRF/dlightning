fa1066bb547000d8f4639d0c061bd9be1c5781ab
GavFior <gavin@woodsofplenty.com>
2023-11-04 19:52:34 -0600

    products fix for mobile.

--- js/main.js
+++ js/main.js
@@ -13,7 +13,15 @@ $(window).on('scroll', function () {
 });
 
 
-
+// function disableScroll() {
+// // Get the current page scroll position in the horizontal direction 
+// scrollLeft = document.documentElement.scrollLeft;
+// // if any scroll is attempted, set this to the previous value
+// window.onscroll = function() {
+// window.scrollTo(scrollLeft);
+// };
+// }
+// disableScroll();
 
 
 $(document).ready(function(){
