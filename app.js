91f89a8ab08444aa588207b513c01b5f5d5bd9b8
GavFior <gavin@woodsofplenty.com>
2023-12-14 22:57:29 -0700

    svganim logo

--- app.js
+++ app.js
@@ -1,50 +1,37 @@
-// const OpenAI = require("openai");
-
-// const openai = new OpenAI();
-
-// async function main() {
-//   console.log("Starting the script");
-//   try {
-//     const completion = await openai.chat.completions.create({
-//       messages: [{ role: "system", content: "You are a helpful assistant." }],
-//       model: "gpt-3.5-turbo",
-//     });
-
-//     console.log("API Response:", completion.choices[0]);
-//   } catch (error) {
-//     console.error("Error:", error);
-//   }
-//   console.log("End of the script");
-// }
-
-// main();
-
 import * as dotenv from 'dotenv';
-import { OpenAI } from "openai";
+import { OpenAI } from 'openai';
+
 dotenv.config();
-const openai = new OpenAI({
-  apiKey: process.env.OPENAI_API_KEY,
-})
 
+async function main() {
+  const openai = new OpenAI({
+    apiKey: process.env.OPENAI_API_KEY2,
+  });
 
+  // Retrieve Assistant
   const assistant = await openai.beta.assistants.retrieve('asst_x1kPyTy2StLakuZt5vy3yZlR');
-  console.log(assistant);
+  console.log('Assistant:', assistant);
 
+  // Create Thread
+  const thread = await openai.beta.threads.create();
+  console.log('Thread:', thread);
 
-// fetchData();
-
-const thread = await openai.beta.threads.create();
-
-const message = await openai.beta.threads.messages.create(thread.id, {
-  role: 'user',
-  content: 'I want to start a cupcake business'
+// Add user message to the thread
+const userMessage = await openai.beta.threads.messages.create(thread.id, {
+  role: 'user', // Make sure 'user' is lowercase here
+  content: 'I want to start a cupcake business',
 });
+console.log('User Message:', userMessage);
 
-const run = await openai.beta.threads.runs.create(thread.id, {
-  assistant_id: assistant.id,
-  instructions: "address the user as Sir William Copperbottom"
-});
+  // Create Assistant message in the thread
+  const assistantMessage = await openai.beta.threads.messages.create(thread.id, {
+    role: 'assistant',
+    content: 'Sure, I can help you with that.',
+  });
+  console.log('Assistant Message:', assistantMessage);
 
-// const run = await openai.beta.threads.runs.retrieve()
+  // Continue the conversation or perform other actions as needed
+}
 
-console.log(run);
+// Call the main function
+main();
