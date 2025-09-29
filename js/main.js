e7220bd091eec823c18de3a06c90f3e9c0f10e54
GavFior <gavin@woodsofplenty.com>
2025-09-29 08:51:26 -0600

    wow out AOS in 404

--- js/main.js
+++ js/main.js
@@ -145,8 +145,7 @@ center: true,
     console.log('ALWAYS - all images have been loaded');
   });
 
-    // wow js
-    new WOW().init();
+    // AOS animations now handled per-page
     
   // counter 
   // $('.counter').counterUp({
@@ -319,22 +318,7 @@ $(document).ready(function() {
 			}
 		}
   });
-  var wow = new WOW(
-    {
-      boxClass:     'wow',      // animated element css class (default is wow)
-      animateClass: 'animated', // animation css class (default is animated)
-      offset:       0,          // distance to the element when triggering the animation (default is 0)
-      mobile:       true,       // trigger animations on mobile devices (default is true)
-      live:         true,       // act on asynchronously loaded content (default is true)
-      callback:     function(box) {
-        // the callback is fired every time an animation is started
-        // the argument that is passed in is the DOM node being animated
-      },
-      scrollContainer: null,    // optional scroll container selector, otherwise use window,
-      resetAnimation: true,     // reset animation on end (default is true)
-    }
-  );
-  wow.init();
+  // WOW animations converted to AOS - initialization handled per-page
 });
 
         // Search Toggle
